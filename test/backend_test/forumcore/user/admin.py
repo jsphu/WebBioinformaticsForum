477466edd492290995a2d5eb9d7d5890b1c602f6
jsphu <yunal22@ogr.iu.edu.tr>
2025-08-14 15:10:20 +0300

    resetting environment

--- test/backend_test/forumcore/user/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserClass
-
-# Register your models here.
-
-class UserClassAdmin(admin.ModelAdmin):
-    list_display = ("username", "created",)
-
-admin.site.register(UserClass, UserClassAdmin)
